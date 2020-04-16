833db0c3e33e49ed8f1e1b537eddbdc2b95bb0d4
eroha <dimon.s186@yandex.ru>
2020-04-16 13:03:39 +0700

    Header and footer

---
+++ resources/views/header_and_footer/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        Сайт разработан веб-студией <a href="https://magneex.com/" class="white-link">Magneex</a>
+    </div>
+</footer>
---
+++ resources/views/header_and_footer/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="container header-container">
+        <div class="header-top">
+            <div class="wrap-message">
+                <div class="message message-item">
+                    <a href="" class="white-link">Написать письмо</a>
+                </div>
+                <div class="dialog message-item">
+                    <a href="" class="white-link">Отзывы и предложения</a>
+                </div>
+            </div>
+            <div class="logo">
+                <img src="{{ 'images/logo.png' }}" alt="logo">
+            </div>
+            <div class="wrap-message">
+                <div class="mortgage message-item">
+                    <a href="" class="black-link">Заявка ипотечному брокеру</a>
+                </div>
+                <div class="realtor message-item">
+                    <a href="" class="black-link">Заявка риэлтору</a>
+                </div>
+            </div>
+        </div>
+        <div class="header-contact-info">
+            <div class="phone"><span>(390)</span> 240 - 55 - 34</div>
+            <div class="street">Черытгашева, 102 <span><a href="">карта проезда</a></span></div>
+        </div>
+       <div class="header-menu">
+           <nav>
+               <ul class="nav-items">
+                   <li class="nav-item"><a href="">О компании</a></li>
+                   <li class="nav-item"><a href="">Новости</a></li>
+                   <li class="nav-item"><a href="">Ипотека</a></li>
+                   <li class="nav-item"><a href="">Аналитиика</a></li>
+                   <li class="nav-item"><a href="">Учебный центр</a></li>
+                   <li class="nav-item"><a href="">Приглашаем в команду</a></li>
+               </ul>
+           </nav>
+       </div>
+    </div>
+</header>
+
